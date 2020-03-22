9a792786593c807924204e3a05829ad3f5567f1a
dotnetCarpenter <jon.ronnenberg@gmail.com>
2020-03-22 13:56:16 +0100

    A Container: Multivalued Functions

--- MultivaluedFunctions.js
+++ MultivaluedFunctions.js
@@ -1,57 +1,50 @@
 // @ts-check
 
-import assert from 'assert'
+import curry from './curry.js'
 import compose from './compose.js'
 
-/** @param {number} n */
-let even = n => n%2!==1
-/** @param {number} n */
-let odd = n => n%2===1
-/** @param {number} root @param {number} n */
-let nthRoot = (root,n) => root<0 && even(n)
-  ? NaN
-  : (root<0
-    ? -1
-    : 1) * Math.abs(root) ** (1/n)
-    // : 1) * Math.pow(Math.abs(root), 1/n)
+const x = 64
 
-let nthRoot1 = (x, n) => n % 2 === 1 && x < 0 ? -(Math.abs(x) ** (1/n)) : x ** (1/n)
+let map = curry((f,x) => x.map(f))
+let concat = curry((x, l) => l.concat(x))
+let sqrt = x => {
+  let sq = Math.sqrt(x)
+  return [sq, -sq]
+}
+let cbrt = x => map(Math.cbrt, x)
 
-/**
- * Calculate nth root of b.
- * root :: (Integral n, RealFloat b) => n -> b -> b
- * @param {number} n
- * @param {number} b
- */
-let root = (b,n) => odd(n) && b<0
-  ? -(Math.abs(b) ** (1/n))
-  : b ** (1/n)
+function main () {
+  console.log(
+    sqrt(64),
+    cbrt([8, -8]),
+    compose(cbrt, sqrt)(x)
+  )
+}
+main()
 
-// root n b | odd n && b < 0  = - abs b ** overn
-//          | otherwise       = b ** overn
-//     where overn = 1 / fromIntegral n
+// let sqrt = Math.sqrt
+let cbrt1 = Math.cbrt
 
-assert( nthRoot(+4, 2) === 2 ) // 2 (the positive is chosen, but -2 is a solution too)
-assert( nthRoot(+8, 3) === 2 ) // 2 (this is the only solution)
-assert( nthRoot(-8, 3) === -2 ) // -2 (this is the only solution)
-assert( Number.isNaN(nthRoot(-4, 2)) ) // NaN (there is no solution)
-assert( Number.isNaN(nthRoot(-8, 4)) ) // NaN (there is no solution)
+// let bind = curry((f, x) => map(f, x).concat(x))
+let bind = curry((f, x) => concat(x, map(f, x)))
+let unit = x => [x]
+let lift = f => compose(unit, f) // lift f = unit . f
 
+function main2 () {
+  console.log(
+    sqrt(64),
+  )
+  console.log(
+    cbrt1([8, -8]),
+    bind(cbrt1)([8, -8]),
+  )
 
-assert( nthRoot1(+4, 2) === 2 ) // 2 (the positive is chosen, but -2 is a solution too)
-assert( nthRoot1(+8, 3) === 2 ) // 2 (this is the only solution)
-assert( nthRoot1(-8, 3) === -2 ) // -2 (this is the only solution)
-assert( Number.isNaN(nthRoot1(-4, 2)) ) // NaN (there is no solution)
-assert( Number.isNaN(nthRoot1(-8, 4)) ) // NaN (there is no solution)
-
-assert( root(+4, 2) === 2 ) // 2 (the positive is chosen, but -2 is a solution too)
-assert( root(+8, 3) === 2 ) // 2 (this is the only solution)
-assert( root(-8, 3) === -2 ) // -2 (this is the only solution)
-assert( Number.isNaN(root(-4, 2)) ) // NaN (there is no solution)
-assert( Number.isNaN(root(-8, 4)) ) // NaN (there is no solution)
-
-// console.log( root(+4, 2) === 2 ) // 2 (the positive is chosen, but -2 is a solution too)
-// console.log( root(+8, 3) === 2 ) // 2 (this is the only solution)
-// console.log( root(-8, 3) === -2 ) // -2 (this is the only solution)
-// console.log( root(-4, 2) ) // NaN (there is no solution)
-// console.log( root(-8, 4) ) // NaN (there is no solution)
+  // lift, lifts a function's return value to the input of bind
+  // bind enable composition of two functions with different input/output
+  console.log(
+    compose(cbrt1, sqrt)(x),
+    compose(bind(cbrt1), sqrt)(x),
+    compose(bind(cbrt1), lift(Math.sqrt))(x),
+  )
+}
+main2()
---
+++ roots.js
@@ -0,0 +1,56 @@
+// @ts-check
+
+import assert from 'assert'
+
+/** @param {number} n */
+let even = n => n%2!==1
+/** @param {number} n */
+let odd = n => n%2===1
+/** @param {number} root @param {number} n */
+let nthRoot = (root,n) => root<0 && even(n)
+  ? NaN
+  : (root<0
+    ? -1
+    : 1) * Math.abs(root) ** (1/n)
+    // : 1) * Math.pow(Math.abs(root), 1/n)
+
+let nthRoot1 = (x, n) => n % 2 === 1 && x < 0 ? -(Math.abs(x) ** (1/n)) : x ** (1/n)
+
+/**
+ * Calculate nth root of b.
+ * root :: (Integral n, RealFloat b) => n -> b -> b
+ * @param {number} n
+ * @param {number} b
+ */
+let root = (b,n) => odd(n) && b<0
+  ? -(Math.abs(b) ** (1/n))
+  : b ** (1/n)
+
+// root n b | odd n && b < 0  = - abs b ** overn
+//          | otherwise       = b ** overn
+//     where overn = 1 / fromIntegral n
+
+assert( nthRoot(+4, 2) === 2 ) // 2 (the positive is chosen, but -2 is a solution too)
+assert( nthRoot(+8, 3) === 2 ) // 2 (this is the only solution)
+assert( nthRoot(-8, 3) === -2 ) // -2 (this is the only solution)
+assert( Number.isNaN(nthRoot(-4, 2)) ) // NaN (there is no solution)
+assert( Number.isNaN(nthRoot(-8, 4)) ) // NaN (there is no solution)
+
+
+assert( nthRoot1(+4, 2) === 2 ) // 2 (the positive is chosen, but -2 is a solution too)
+assert( nthRoot1(+8, 3) === 2 ) // 2 (this is the only solution)
+assert( nthRoot1(-8, 3) === -2 ) // -2 (this is the only solution)
+assert( Number.isNaN(nthRoot1(-4, 2)) ) // NaN (there is no solution)
+assert( Number.isNaN(nthRoot1(-8, 4)) ) // NaN (there is no solution)
+
+assert( root(+4, 2) === 2 ) // 2 (the positive is chosen, but -2 is a solution too)
+assert( root(+8, 3) === 2 ) // 2 (this is the only solution)
+assert( root(-8, 3) === -2 ) // -2 (this is the only solution)
+assert( Number.isNaN(root(-4, 2)) ) // NaN (there is no solution)
+assert( Number.isNaN(root(-8, 4)) ) // NaN (there is no solution)
+
+// console.log( root(+4, 2) === 2 ) // 2 (the positive is chosen, but -2 is a solution too)
+console.log( Math.cbrt(+8) ) // 2 (this is the only solution)
+console.log( Math.cbrt(-8) ) // -2 (this is the only solution)
+// console.log( root(-4, 2) ) // NaN (there is no solution)
+// console.log( root(-8, 4) ) // NaN (there is no solution)
